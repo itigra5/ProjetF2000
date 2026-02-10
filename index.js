6e7bb2d1978a968cbcabd5ac105088e5da37c892
itigra5 <ilhem.mezaache22@gmail.com>
2026-02-10 22:58:16 +0100

    1er

--- index.js
+++
@@ -1,8 +0,0 @@
-import http from "http";
-
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello Render");
-}).listen(PORT);
