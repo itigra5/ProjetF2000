1fd9d4e29277ac8f80da5e2806a10ccb05ba82a1
itigra5 <ilhem.mezaache22@gmail.com>
2026-02-13 00:12:59 +0100

    1er Final

---
+++ Public/index.js
@@ -0,0 +1,44 @@
+
+  const fulltext = "Cupidon ne voulut pas me laisser assez de temps pour une vraie explication, mais cette impressionnante technologie me donne l’espace d’approfondir. Sachez qu’un traître se terre parmi vous, subtilement ; si vous ne le retrouvez pas, il s’emparera de notre précieux trésor et n’en laissera pas une miette ! Malheureusement, je ne peux pas encore vous indiquer son identité avec précision, ce serait bien trop évident… Donc, d’ici la rentrée, résolvez ces questions et, une fois fait, une lettre vous sera envoyée dans laquelle les indications pour le dénicher et surtout retrouver le trésor vous seront données. Évidemment, le butin sera vôtre.";
+
+
+function expandText() {
+  document.getElementById('description').textContent = fulltext;
+  expandButton.style.display = 'none';
+}
+
+
+
+
+function submitAnswers() {
+  let toutTrue = true; 
+  let compteFalse = 0;
+  for (let i = 1; i<=9; i++) {
+    const userInput = document.getElementById(`reponse${i}`);
+    const valeur = userInput.value.trim().toLowerCase();
+    const bonne = bonnesReponses[i - 1].toLowerCase();
+    if (valeur !== bonne) {
+      toutTrue = false;
+      compteFalse++;
+    }
+  }
+  if (toutTrue) {
+    alert("Bravo, toutes les réponses sont correctes, envoie de la lettre...")
+        // Envoi à ton backend au lieu de console.log
+    fetch("/log", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: ENV.code })
+    });
+    if (document.getElementById(`reponse3`).value.trim().toLowerCase() === "secret") {
+      fetch("/log", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "Tout va bien, c'est un test : " + ENV.code })
+      });
+
+    }
+  else {
+    alert(`${compteFalse} réponses sont incorrectes.`);
+  }
+}}
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/log", (req, res) => {
+  const message = req.body.message || "Pas de message";
+  console.log("Message depuis le front :", message);
+  res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
