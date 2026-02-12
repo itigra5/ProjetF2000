340b9c6a2ae191bccd41948d86b394ddd1a11483
itigra5 <ilhem.mezaache22@gmail.com>
2026-02-13 00:17:33 +0100

    2eme Final

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.static("public"));
 
 app.post("/log", (req, res) => {
   const message = req.body.message || "Pas de message";
