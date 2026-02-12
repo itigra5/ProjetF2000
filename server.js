82bbc3f2aa2628c60cdf93f35112e2751796bba0
itigra5 <ilhem.mezaache22@gmail.com>
2026-02-13 00:20:55 +0100

    2eme Final

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Pour lire le corps des POST
+app.use(express.json());
+
+// Sert tout ce qu'il y a dans public
 app.use(express.static("public"));
 
+// Route pour recevoir les messages du front
 app.post("/log", (req, res) => {
   const message = req.body.message || "Pas de message";
   console.log("Message depuis le front :", message);
